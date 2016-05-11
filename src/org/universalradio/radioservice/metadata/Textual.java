a7272df7000ba08d731ff8db7926bfa18b783120
Fabian Sattler <sattler@irt.de>
2016-05-11 12:32:26 +0200

    Changed license to Apache v2

--- src/org/universalradio/radioservice/metadata/Textual.java
+++ src/org/universalradio/radioservice/metadata/Textual.java
@@ -1,10 +1,22 @@
 package org.universalradio.radioservice.metadata;
 
 /**
- * Copyright (C) IRT - Institut für Rundfunktechnik 2015
+ * Copyright (C) 2016 Open Mobile Radio Interface (OMRI) Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * 
  * Abstract base class for textual metadata
- * @author Sattler, IRT GmbH
+ * @author Fabian Sattler, IRT GmbH
  */
 public interface Textual {
 
